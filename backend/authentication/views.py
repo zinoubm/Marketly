b0c59d8de3ef3aa5fb80b328cf52e09ba3ab0d2b
zinoubm <zinne.eddine.2000@gmail.com>
2024-04-19 06:39:42 +0100

    customizing auth endpoints

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,15 +1,18 @@
-from django.conf import settings
-from django.http import HttpResponse
-
-from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
-from rest_framework import status
+from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView
-from .serializers import GoogleLoginSerializer
+from .serializers import (
+    UserRegisterSerializer,
+    UserLoginSerializer,
+    GoogleLoginSerializer,
+)
 
-from google.oauth2 import id_token
-from google.auth.transport import requests
+
+class UserRegisterViwe(RegisterView):
+    serializer_class = UserRegisterSerializer
+
+
+class UserLoginView(LoginView):
+    serializer_class = UserLoginSerializer
 
 
 class GoogleLoginView(LoginView):
