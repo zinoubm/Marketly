f7dc7d6bdb8ebef6dc91500ebb86242a6c1a9461
zinoubm <zinne.eddine.2000@gmail.com>
2024-04-30 15:45:59 +0100

    makin buyer read only field for Order serializer

--- backend/marketly/serializers/order.py
+++ backend/marketly/serializers/order.py
@@ -9,4 +9,4 @@ class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = "__all__"
-        read_only_fields = ["status"]
+        read_only_fields = ["status", "buyer"]
