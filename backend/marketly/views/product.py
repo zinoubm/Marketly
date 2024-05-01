9571de242379b85dd979b693e0aae53af48e5627
zinoubm <zinne.eddine.2000@gmail.com>
2024-05-01 10:40:07 +0100

    adding search

--- backend/marketly/views/product.py
+++ backend/marketly/views/product.py
@@ -1,5 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework import filters
+
 
 from marketly.models import Product
 from marketly.serializers import ProductSerializer
@@ -8,8 +10,9 @@ from marketly.serializers import ProductSerializer
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     parser_classes = (MultiPartParser, FormParser)
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
 
-    # queryset = Product.objects.filter(sel)
     serializer_class = ProductSerializer
     def get_queryset(self):
         return Product.objects.filter(seller=self.request.user)
@@ -24,3 +27,6 @@ class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView)
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+# add a view for search and filtering
