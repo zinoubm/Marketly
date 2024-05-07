0ee352357023127e880787383c3e5e4d02555edf
mc <mohcench9@gmail.com>
2024-05-07 17:26:41 +0100

    fix some stuff and customize the endpoints more

--- backend/marketly/views/notification.py
+++ backend/marketly/views/notification.py
@@ -1,13 +1,41 @@
 from rest_framework import generics
 from marketly.models import Notification
 from marketly.serializers import NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+from drf_spectacular.utils import extend_schema
 
 
 class NotificationListCreateAPIView(generics.ListCreateAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
 
 
-class NotificationDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Notification.objects.all()
-    serializer_class = NotificationSerializer
+class NotificationMarkIsSeenView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        request={"id": 0},
+        responses={200: {"success": ""}},
+        description="Make a PATCH request and Give it the id of the notification in the data object and the view will turn is_seen to True",
+    )
+    def patch(self, request, *args, **kwargs):
+        notification_id = request.data.get("id")
+        if not notification_id:
+            return Response(
+                {"id": "This field is required"}, status=HTTP_400_BAD_REQUEST
+            )
+        try:
+            notification = Notification.objects.get(id=notification_id)
+        except Notification.DoesNotExist:
+            return Response(
+                {"error": "Notification not found"}, status=HTTP_404_NOT_FOUND
+            )
+        notification = Notification.objects.get(id=notification_id)
+        notification.is_seen = True
+        notification.save()
+        return Response({"success": "Notification marked as seen"}, status=HTTP_200_OK)
--- backend/marketly/views/review.py
+++ backend/marketly/views/review.py
@@ -1,16 +1,27 @@
 from rest_framework import generics
 from marketly.models import Review
 from marketly.serializers import ReviewSerializer
+from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema
 
 
 class ReviewListCreateAPIView(generics.ListCreateAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    @extend_schema(
+        description="API endpoint create reviews or get reviews for a specific product by passing the product_id as querystring ex: ?product_id=3"
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class ReviewDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Review.objects.all()
-    serializer_class = ReviewSerializer
+    def get_queryset(self):
+        product_id = self.request.query_params.get("product_id")
+        if product_id:
+            return Review.objects.filter(product_id=int(product_id))
+        else:
+            return Review.objects.all()
