cdbed6605d47d533df8599c781475dfa2191fa57
Ramache Zoubir <ramachezoubir@gmaail.com>
2024-05-09 09:42:54 +0100

    feat(Search): add search page with filters : category , min max price  , did not add Rating filter

---
+++ frontend/src/pages/search/SearchPage.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import Filters from "./Filters";
+import TopBar from "../home/TopBar";
+import { useSearchParams } from "react-router-dom";
+import useProductApi from "@/lib/api/useProductApi";
+import Card from "@/components/shared/productCard";
+function SearchPage() {
+  const { searchForProduct } = useProductApi();
+  const [URLSearchParams, setSearchParams] = useSearchParams();
+  const [products , setProducts]=useState([])
+  useEffect(() => {
+    (async () => {
+      const searchObject ={}
+      for(const [key , value]of URLSearchParams.entries()){
+        searchObject[key]=value
+      }
+      const data = await searchForProduct(searchObject);
+      setProducts(data)
+      
+    })();
+  }, []);
+  return (
+    <div>
+      <TopBar />
+      <Filters />
+      <div className=" flex gap-4 px-2 flex-wrap ">
+
+      {products.length ?products.map(prod=>(<Card {...prod} key={prod.id}/>)) : "there is no product here"}
+      </div>
+    </div>
+  );
+}
+
+export default SearchPage;
